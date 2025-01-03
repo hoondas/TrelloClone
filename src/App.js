e3cbaa53eca83ea7bbb548c6e18f63894380410f
hoondas <guilhemhonda@gmail.com>
2025-01-03 11:33:42 -0300

    Dragging and dropping implemented in different columns

--- src/App.js
+++ src/App.js
@@ -20,32 +20,51 @@ const initialColumns = [
     id: "456",
     items:[],
   },
+  {
+    name: "Done",
+    id: "789",
+    items:[],
+  },
 ];
 
 function App() {
   const [columns, setColumns] = useState(initialColumns);
 
   const onDragEnd = (result) => {
-    var sourceColumnItems = columns[0].items;
+    var sourceIndex = 0;
+    var destinationIndex = 0;
     var draggedItem = {};
 
+    for (var i in columns) {
+      if (result.source.droppableId == columns[i].id) {
+        var sourceColumnItems = columns[i].items;
+        sourceIndex = i;
+      } else if (result.destination.droppableId == columns[i].id) {
+        var destinationColumnItems = [...columns[i].items];
+        destinationIndex = i;
+      }
+    }
+
+    // Find the dragged item
     for (var i in sourceColumnItems) {
       if (sourceColumnItems[i].id == result.draggableId) {
         draggedItem = sourceColumnItems[i];
       }
     }
+
     // "Excluding" the dragged object by filtering  
     var filteredSourceColumnItems = sourceColumnItems.filter((item) => item.id != result.draggableId);
 
-    // Adding it to the new position
-    filteredSourceColumnItems.splice(result.destination.index, 0, draggedItem);
-    console.log(filteredSourceColumnItems);
-    console.log(result);
-
-    // Changing the state
+    // Adding it to the new position and changing the state
     var columnsCopy = JSON.parse(JSON.stringify(columns));
-    columnsCopy[0].items = filteredSourceColumnItems;
-    setColumns(columnsCopy); 
+    if (result.source.droppableId == result.destination.droppableId) {
+      filteredSourceColumnItems.splice(result.destination.index, 0, draggedItem)
+    } else {
+      destinationColumnItems.splice(result.destination.index, 0, draggedItem);
+      columnsCopy[destinationIndex].items = destinationColumnItems;
+    }
+    columnsCopy[sourceIndex].items = filteredSourceColumnItems;
+    setColumns(columnsCopy);
   }
 
   return (
